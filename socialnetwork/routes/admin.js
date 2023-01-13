1868ec2249a730a9212cc9c44b5de6411f2e7042
UO258418 <UO258418@uniovi.es>
2023-01-13 18:46:14 +0100

    Duplicate almost working

--- socialnetwork/routes/admin.js
+++ socialnetwork/routes/admin.js
@@ -125,7 +125,118 @@ module.exports = function (app, usersRepository, friendsRepository, publications
     }
 
 
+    app.get('/admin/duplicate/:id', function (req, res) {
+        logger.info("[GET] /admin/duplicate");
 
+        let filter = {_id: ObjectId(req.params.id)};
+        usersRepository.findUser(filter, {}).then(user => {
+            console.log(user)
+            res.render("admin/duplicate.twig", {user: user});
+        })
+
+    })
+
+    app.post('/admin/duplicate/:id', function (req, res) {
+        logger.info("[POST] /admin/duplicate/"+req.params.id);
+
+        let id = req.params.id;
+
+        let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+
+        let userValidate = {
+            email: req.body.email,
+            name: req.body.name,
+            surname: req.body.surname,
+            password: req.body.password,
+            passwordConfirm : req.body.passwordConfirm
+        };
+
+        validateDuplicate(userValidate, function(errors){
+            if (errors!=null && errors.length>0){
+                logger.error("[POST] /admin/duplicate - " + errors);
+                res.redirect("/admin/duplicate/" + id +
+                    "?message="+errors+"&messageType=alert-danger");
+            } else {
+                let user = {
+                    email: req.body.email,
+                    name: req.body.name,
+                    surname: req.body.surname,
+                    password: securePassword,
+                    role:"standard"
+                };
+
+                usersRepository.getUsers({email: req.body.email}, {}).then( users => {
+                    if (users != null && users.length != 0){
+                        logger.error("[POST] /admin/duplicate/ - Email is already in use");
+                        res.redirect("/admin/duplicate/" + id +
+                            "?message=Email is already in use"+
+                            "&messageType=alert-danger");
+                    } else {
+                        usersRepository.insertUser(user).then(userId => {
+                            //todo Redirigir a las opciones de usuario
+                            res.redirect("/admin/list" + "?message=New user successfully registered" +
+                                "&messageType=alert-success");
+                        }).catch(error => {
+                            logger.error("[POST] /admin/duplicate - An error has occurred adding the user");
+                            res.redirect("/admin/duplicate" +
+                                "?message=An error has occurred adding the user"+
+                                "&messageType=alert-danger");
+                        });
+                    }
+                }).catch(error => {
+                    logger.error("[POST] /admin/duplicate - An error has occurred");
+                    res.redirect("/admin/duplicate" + id +
+                        "?message=An error has occurred"+
+                        "&messageType=alert-danger");
+                });
+
+            }
+        })
+
+
+
+
+    });
+
+
+    function validateDuplicate(user, callback){
+        let errors = new Array();
+        //Name
+        if (user.name === null || typeof user.name === 'undefined' ||user.name.length<4 ||user.name.length>20|| user.name.trim().length == 0){
+            errors.push("Name must be between 4 and 20 characters, it cannot be empty");
+        }
+
+        //Surname
+        if (user.surname === null || typeof user.surname === 'undefined' ||user.surname.length<4 ||user.surname.length>20|| user.surname.trim().length == 0){
+            errors.push("Surname must be between 4 and 20 characters. It cannot be empty");
+        }
+
+        //Email
+        if (user.email === null || typeof user.email === 'undefined' || user.email.trim().length == 0){
+            errors.push("Email cannot be empty");
+        }
+        //testing the email has the correct format
+        let pattern = /\S+@\S+\.\S+/;
+        if (! pattern.test(user.email)){
+            errors.push("Email does not follow the expected format");
+        }
+
+        //Password
+        if (user.password !== user.passwordConfirm){
+            errors.push("Passwords do not match");
+        }
+        if (user.password === null || typeof user.password === 'undefined' ||user.password.length<5 ||user.password.length>8|| user.password.trim().length == 0){
+            errors.push("Password must be between 5 and 8 characters");
+        }
+
+
+        if (errors.length<=0){
+            callback(null);
+        } else {
+            callback(errors);
+        }
+    }
 
 
 }
